728b5050afa3593c2475bd9371b5e936d49c308b
menelmacar <92785861+Menelmacar0904@users.noreply.github.com>
2026-04-18 23:27:14 +0800

    Update TXT to EPUB Converter.py
    优化了简介的标题设置逻辑

--- TXT to EPUB Converter.py
+++ TXT to EPUB Converter.py
@@ -7,7 +7,6 @@ from tkinter import filedialog
 from tkinter import messagebox
 import ctypes
 
-
 # 高DPI适配
 try:
     # 针对 Windows 8.1 及以上版本
@@ -23,7 +22,7 @@ except Exception:
 root = tk.Tk()
 root.title('TXT转EPUB工具')
 root.geometry('0x0')
-root.attributes('-alpha', 0)      # 把主窗口变为透明，但仍能保持任务栏图标
+root.attributes('-alpha', 0)  # 把主窗口变为透明，但仍能保持任务栏图标
 
 # 打开目标txt文件
 while True:
@@ -31,7 +30,8 @@ while True:
     if txt_file == '':
         messagebox.showwarning(title='未选取文件', message='未选取文件！\n程序将自动退出。')
         sys.exit()
-    else: break
+    else:
+        break
 
 # 检测目标文件编码
 target_encoding = 'utf-8'
@@ -39,14 +39,13 @@ target_errors = 'strict'
 try:
     with open(txt_file, encoding=target_encoding) as test:
         test.read(8192)
-except UnicodeDecodeError:          # 如果 utf-8 报错，尝试用 gbk 读取
+except UnicodeDecodeError:  # 如果 utf-8 报错，尝试用 gbk 读取
     target_encoding = 'gbk'
     target_errors = 'ignore'
 
-
 # 正则表达式预编译
-p_h1 = re.compile('[【\\[]?第\\s*[零一二两三四五六七八九十百千\\d]+\\s*卷(\\s|$)|'
-                  '[【\\[]?简介[]】]?[:：\\s]')
+p_h1 = re.compile('[【\\[]?第\\s*[零一二两三四五六七八九十百千\\d]+\\s*卷(\\s|$)')
+p_intro = re.compile('[【\\[]?简介[]】]?[:：\\s]')
 p_h2 = re.compile('[【\\[]?第\\s*[零一二两三四五六七八九十百千\\d]+\\s*[章回节集](\\s|$)|'
                   '[\\[【]?彩蛋[：\\s]|'
                   '[\\[【]?番外[：\\s]|'
@@ -57,8 +56,9 @@ title_chars = set('第简彩番''0123456789''零一二两三四五六七八九�
 book_stem = Path(txt_file).stem
 book_author = ''
 markdown_lines = []
-mark = 0            # 正文标记
-title_layer = 1         # 标题层级记录
+mark = 0  # 正文标记
+intro = 0  # 简介标记
+title_layer = 1  # 标题层级记录
 
 with open(txt_file, 'r', encoding=target_encoding, errors=target_errors) as f_in:
     for line in f_in:
@@ -69,20 +69,25 @@ with open(txt_file, 'r', encoding=target_encoding, errors=target_errors) as f_in
             match = p_author_extract.search(line)
             if match: book_author = match.group(1).strip()
         # 添加标题
-        if clean_line[0] not in title_chars or len(clean_line) > 40: pass
-        elif p_h1.match(clean_line):  # 给简介与整卷添加大标题
+        if clean_line[0] not in title_chars or len(clean_line) > 40:
+            pass
+        elif intro == 0 and p_intro.match(clean_line):  # 为简介添加大标题
+            clean_line = '# ' + clean_line
+            mark = 1
+            intro = 1
+        elif p_h1.match(clean_line):  # 给整卷添加大标题
             clean_line = '# ' + clean_line
             title_layer = 2
             mark = 1
         elif p_h2.match(clean_line):  # 检测章节标题
-            clean_line= title_layer * '#' + ' ' + clean_line
+            clean_line = title_layer * '#' + ' ' + clean_line
             mark = 1
         if mark:
             markdown_lines.append(f"{clean_line}\n\n")
 
 markdown_text = ''.join(markdown_lines)
 
-epub_file = Path(txt_file).parent / f'{book_stem}.epub'         # 输出文件转换
+epub_file = Path(txt_file).parent / f'{book_stem}.epub'  # 输出文件转换
 
 css_file = Path(txt_file).parent / "style.css"
 with open(css_file, "w", encoding="utf-8") as css:
@@ -130,16 +135,17 @@ with open(css_file, "w", encoding="utf-8") as css:
 
 # EPUB文件转换
 pypandoc.convert_text(
-    source= markdown_text,
-    to = 'epub',
-    format = 'commonmark',
-    outputfile = str(epub_file),
-    extra_args = ['--metadata', f'title={book_stem}',
-                  '--metadata', f'author={book_author}',
-                  '--toc', '--toc-depth=2', '--split-level=2',
-                  '--metadata', 'toc-title=目录',
-                  f'--css={css_file}']
+    source=markdown_text,
+    to='epub',
+    format='commonmark',
+    outputfile=str(epub_file),
+    extra_args=['--metadata', f'title={book_stem}',
+                '--metadata', f'author={book_author}',
+                '--toc', '--toc-depth=2', '--split-level=2',
+                '--metadata', 'toc-title=目录',
+                f'--css={css_file}']
 )
 
 css_file.unlink()
-messagebox.showinfo(title='转换成功', message=f'恭喜！《{book_stem}》已成功转换为 EPUB 电子书！\n文件已保存为：\n{epub_file}')
+messagebox.showinfo(title='转换成功',
+                    message=f'恭喜！《{book_stem}》已成功转换为 EPUB 电子书！\n文件已保存为：\n{epub_file}')
